01bf68cacf4c7f38ea1361f277063bd420b847ba
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-09-17 19:02:30 -0400

    Quick border addition to the sidebar.

--- nebulavault-frontend/src/app/Dashboard/page.tsx
+++ nebulavault-frontend/src/app/Dashboard/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div className="bg-secondary h-full">page</div>;
+  return <div className="h-full">page</div>;
 };
 
 export default page;
--- nebulavault-frontend/src/components/MainSidebar.tsx
+++ nebulavault-frontend/src/components/MainSidebar.tsx
@@ -8,7 +8,7 @@ const MainSidebar = () => {
   const [pageSelected, setPageSelected] = useState("Dashboard");
 
   return (
-    <div className="flex flex-col items-center gap-5 py-10 p-2">
+    <div className="flex flex-col items-center gap-5 py-10 p-2 h-full border-2 border-[#15161d]">
       <section className="w-full">
         <SideBarTitleSection />
       </section>
