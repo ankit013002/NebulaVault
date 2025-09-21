bfdaf2a4889bef7392271c37cfefb288d94e8dd3
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-09-21 18:34:14 -0400

    Some additions to start getting things configured. Also had the spring cloud communicate to a flask server to test.

--- nebula-gateway/src/main/java/com/nebulavault/gateway/NebulaGatewayApplication.java
+++ nebula-gateway/src/main/java/com/nebulavault/gateway/NebulaGatewayApplication.java
@@ -2,6 +2,9 @@ package com.nebulavault.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NebulaGatewayApplication {
@@ -9,5 +12,17 @@ public class NebulaGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NebulaGatewayApplication.class, args);
 	}
+
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/files/**")
+                        .filters(f -> f
+                                .prefixPath("/api")
+                                .addResponseHeader("Nebula-Gateway","Nebula Vault")
+                        ).uri("http://192.168.0.215:5000")
+                ).build();
+    }
 
 }
