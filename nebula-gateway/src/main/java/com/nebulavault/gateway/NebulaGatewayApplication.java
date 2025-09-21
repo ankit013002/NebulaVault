5e6512764a383bdf59ae3906b65897f4d08ffe39
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-09-21 16:40:51 -0400

    Initial commit of spring cloud.

---
+++ nebula-gateway/src/main/java/com/nebulavault/gateway/NebulaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nebulavault.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebulaGatewayApplication.class, args);
+	}
+
+}
