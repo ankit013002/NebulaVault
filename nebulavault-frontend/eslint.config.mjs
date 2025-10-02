76b448c9303cc6e67b2292559385fe8bb0f3a9b1
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-10-01 20:55:27 -0400

    Resolving issues related to linting in the frontend for the CI.

--- nebulavault-frontend/eslint.config.mjs
+++ nebulavault-frontend/eslint.config.mjs
@@ -20,6 +20,20 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
