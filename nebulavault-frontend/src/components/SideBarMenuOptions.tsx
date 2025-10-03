2db6e8a86f19e9bd623d1fc4706f47b7b8c47382
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-10-02 20:37:47 -0400

    Hot fix to the sidebar. Correct sidebar option was not being highlighted.

--- nebulavault-frontend/src/components/SideBarMenuOptions.tsx
+++ nebulavault-frontend/src/components/SideBarMenuOptions.tsx
@@ -1,11 +1,23 @@
 "use client";
 
 import { SideBarOptions } from "@/utils/SideBarOptions";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SideBarMenuOption from "./SideBarMenuOption";
+import { usePathname } from "next/navigation";
 
 const SideBarMenuOptions = () => {
-  const [pageSelected, setPageSelected] = useState("");
+  const pathname = usePathname();
+  const [pageSelected, setPageSelected] = useState("Dashboard");
+
+  useEffect(() => {
+    if (pathname) {
+      const segments = pathname.split("/").filter(Boolean);
+      const current = segments[segments.length - 1] || "dashboard";
+      const formatted = current.charAt(0).toUpperCase() + current.slice(1);
+
+      setPageSelected(formatted);
+    }
+  }, [pathname]);
 
   return (
     <div className="flex flex-col w-full">
